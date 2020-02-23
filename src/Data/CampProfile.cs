41de8316bf4128f18d984069a081d263b987ad81
apatel369 <33442948+apatel369@users.noreply.github.com>
2020-02-23 15:43:32 -0600

    return related data using auto mapper

--- src/Data/CampProfile.cs
+++ src/Data/CampProfile.cs
@@ -11,7 +11,8 @@ namespace CoreCodeCamp.Data
     {
         public CampProfile()
         {
-            this.CreateMap<Camp, CampModel>();
+            this.CreateMap<Camp, CampModel>()
+                .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName));
         }
     }
 }
