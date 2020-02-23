cb2e338574d3c86e1a2925075c804c1eebdbd729
apatel369 <33442948+apatel369@users.noreply.github.com>
2020-02-23 15:14:40 -0600

    get indivitual camp endpoint

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -36,5 +36,21 @@ namespace CoreCodeCamp.Controllers
             }
             
         }
+        [HttpGet("{moniker}")]
+        public async Task<ActionResult<CampModel>> Get(string moniker)
+        {
+            try
+            {
+                var result = await _repository.GetCampAsync(moniker);
+
+                if (result == null) return NotFound();
+
+                return _mapper.Map<CampModel>(result);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
     }
 }
