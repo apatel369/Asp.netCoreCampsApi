0c9c600fbb6e74ff5baa08c05458369cb0d2d4cd
apatel369 <33442948+apatel369@users.noreply.github.com>
2020-02-23 21:26:05 -0600

    implement PUT

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -108,5 +108,27 @@ namespace CoreCodeCamp.Controllers
             }
             return BadRequest();
         }
+
+        [HttpPut("{moniker}")]
+        public async Task<ActionResult<CampModel>> Put(string moniker, CampModel model)
+        {
+            try
+            {
+                var oldCamp = await _repository.GetCampAsync(model.Moniker);
+                if (oldCamp == null) return NotFound($"Could not find camp with moniker of {moniker}");
+
+                _mapper.Map(model, oldCamp);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    return _mapper.Map<CampModel>(oldCamp);
+                }
+                return BadRequest("Could not update");
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
     }
 }
