14cafc8a892f3c6ee098539098bda991db2b19fa
apatel369 <33442948+apatel369@users.noreply.github.com>
2020-02-23 21:52:54 -0600

    implement DELETE

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -130,5 +130,27 @@ namespace CoreCodeCamp.Controllers
                 return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
             }
         }
+        [HttpDelete("{moniker}")]
+        public async Task<IActionResult> Delete(string moniker)
+        {
+            try
+            {
+                var oldCamp = await _repository.GetCampAsync(moniker);
+                if (oldCamp == null) return NotFound();
+
+                _repository.Delete(oldCamp);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Ok();
+                }
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
+            return BadRequest("Could not delete");
+        }
     }
 }
